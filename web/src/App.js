e8923128cec224bd6136ef7dffff9ccbaf027f94
Willians <wrgoncalves2008@gmail.com>
2020-01-16 00:49:31 -0300

    Criado arquivos de estilos

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './global.css';
+import './App.css';
 
 function App() {
+
   return (
-    <h1>Hello World</h1>
+    <div id="app">
+
+      <aside>
+
+      </aside>
+
+      <main>
+
+
+      </main>
+
+    </div>
   );
 }
 
