5c01b949b4eeaafdb72852e25b639d4ff645ceb0
Willians <wrgoncalves2008@gmail.com>
2020-01-17 00:58:12 -0300

    Finalizado a parte web da aplicação

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import DevItem from './Components/DevItem';
+import DevForm from './Components/DevForm';
+
 import './global.css';
 import './App.css';
+import './Main.css';
 
 function App() {
+
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    };
+
+    loadDevs();
+
+  }, [])
+
+  async function handleAddDev(data) {
+
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
 
       <aside>
-
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
-
-
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
       </main>
 
-    </div>
+    </div >
   );
 }
 
